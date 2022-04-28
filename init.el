1c0e77bb03216946f9556699973c9646e0656772
Yoshida.Eiji <yoshida.eiji@trans-cosmos.co.jp>
2022-04-28 16:32:20 +0900

    カレンダーとnyan-mode追加

--- init.el
+++ init.el
@@ -398,3 +398,38 @@
 (when (equal system-type 'windows-nt)
   (setq find-program "c:/GnuWin/bin/find.exe")
   (setq grep-program "c:/GnuWin/bin/grep.exe"))
+
+;; nyan-mode
+(require 'nyan-mode)
+(nyan-mode)
+(setq-default nyan-wavy-trail t)
+(nyan-start-animation)
+
+;; 日本の祝日
+(with-eval-after-load "calendar"
+  (require 'japanese-holidays)
+  (setq calendar-holidays ; 他の国の祝日も表示させたい場合は適当に調整
+        (append japanese-holidays holiday-local-holidays holiday-other-holidays))
+  (setq calendar-mark-holidays-flag t)	; 祝日をカレンダーに表示
+  ;; 土曜日・日曜日を祝日として表示する場合、以下の設定を追加します。
+  ;; デフォルトで設定済み
+  (setq japanese-holiday-weekend '(0 6)	   ; 土日を祝日として表示
+        japanese-holiday-weekend-marker	   ; 土曜日を水色で表示
+        '(holiday nil nil nil nil nil japanese-holiday-saturday))
+  (add-hook 'calendar-today-visible-hook 'japanese-holiday-mark-weekend)
+  (add-hook 'calendar-today-invisible-hook 'japanese-holiday-mark-weekend)
+  (add-hook 'calendar-today-visible-hook 'calendar-mark-today)
+  (setq calendar-month-header '(propertize
+                                (format "%d年 %s月" year month)
+                                'font-lock-face 'calendar-month-header))
+  (let ((array ["日" "月" "火" "水" "木" "金" "土"]))
+    (setq calendar-day-header-array array
+          calendar-day-name-array array))
+  (defun my/japanese-holiday-show (&rest _args)
+    (let* ((date (calendar-cursor-to-date t))
+           (calendar-date-display-form '((format "%s年 %s月 %s日（%s）" year month day dayname)))
+           (date-string (calendar-date-string date))
+           (holiday-list (calendar-check-holidays date)))
+      (when holiday-list
+        (message "%s: %s" date-string (mapconcat #'identity holiday-list "; ")))))
+  (add-hook 'calendar-move-hook 'my/japanese-holiday-show))
